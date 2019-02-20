b85c28e51ccbb361c944b19e15675a288b3de101
Rishav Bhagat <rishavb123@gmail.com>
2019-02-20 17:02:37 -0500

    trying to get velocity to output

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-let totalAcceleration = 0;
-let count = 0;
-
 let first = true;
+let startTime = new Date().getTime();
+let curTime = startTime;
+
+let v = 0;
 
 if (window.DeviceMotionEvent != undefined) {
     window.ondevicemotion = (e) => {
         document.getElementById("p1").innerHTML = e.acceleration.x;
         document.getElementById("p2").innerHTML = e.acceleration.y;
         document.getElementById("p3").innerHTML = e.acceleration.z;
-        if (!first) {
-            totalAcceleration += e.acceleration.x;
-            count++;
-        }
 
 
-        document.getElementById("p4").innerHTML = totalAcceleration / count;
+
+        const deltaT = (new Date().getTime() - curTime) / 1000.0;
+        v += e.acceleration.x * deltaT;
+        document.getElementById("p4").innerHTML = Math.round(v);
 
         first = false;
 
