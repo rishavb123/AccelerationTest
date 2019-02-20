158541e769519ed74553715db5c8fe051ebb75d9
Rishav Bhagat <rishavb123@gmail.com>
2019-02-20 17:19:16 -0500

    outputing v on reset

--- main.js
+++ main.js
@@ -10,15 +10,16 @@ if (window.DeviceMotionEvent != undefined) {
         document.getElementById("p2").innerHTML = e.acceleration.y;
         document.getElementById("p3").innerHTML = e.acceleration.z;
 
-
-
         const deltaT = (new Date().getTime() - curTime) / 1000.0;
         v += Math.round(e.acceleration.x * 10) / 10.0 * deltaT;
-        document.getElementById("p4").innerHTML = Math.round(v);
+        document.getElementById("p4").innerHTML = -Math.round(v);
 
         first = false;
 
     }
 }
 
-document.getElementById("reset").onclick = () => { v = 0; };
+document.getElementById("reset").onclick = () => {
+    v = 0;
+    document.getElementById("p4").innerHTML = Math.round(v)
+};
