6e746c742b7c6e3e249bd65c290d0d74677612c2
KihtrakRaknas <karthikandsankar@gmail.com>
2019-02-20 19:04:18 -0500

    added circle function

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var c = document.getElementById("can");
 var ctx = c.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
-
-function Cir(){
-
+function Cir(x,y,radius){
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.stroke();
 }
+
+
+Cir(50,50,5);
+
+Cir(50,50,10);
+
+//x,y,radius,start angle, end angle
+//ctx.arc(95, 50, 40, 0, 2 * Math.PI);
