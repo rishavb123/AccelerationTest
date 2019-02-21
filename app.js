5cd312e7e3048b3c67436e6ea4c090452967ec8f
KihtrakRaknas <karthikandsankar@gmail.com>
2019-02-20 19:12:15 -0500

    added col of Circles

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var ctx = c.getContext("2d");
 c.width = document.body.clientWidth; //document.width is obsolete
 c.height = document.body.clientHeight; //document.height is obsolete
 
-function Cir(x,y,radius){
+function cir(x,y,radius){
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
+function colOfCircles(numOfCircles,x){
+  for(var i =0; i!=numOfCircles; i++){
+    cir(c.height/numOfCircles,x,50);
+  }
+}
 
-Cir(50,50,5);
-
-Cir(50,50,10);
+colOfCircles(3,100)
 
 //x,y,radius,start angle, end angle
 //ctx.arc(95, 50, 40, 0, 2 * Math.PI);
